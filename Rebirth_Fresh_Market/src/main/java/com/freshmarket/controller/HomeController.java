b285e97b6d75a08d4c2aa376331fb56aab81fcee
latentdark <latentdark@mensakorea.org>
2014-05-15 20:34:26 +0900

    google.jsp연동

--- Rebirth_Fresh_Market/src/main/java/com/freshmarket/controller/HomeController.java
+++ Rebirth_Fresh_Market/src/main/java/com/freshmarket/controller/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Handles requests for the application home page.
@@ -20,10 +21,27 @@ public class HomeController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	
+	@RequestMapping(value = "/googleMap")
+	public ModelAndView googlemap(Locale locale, Model model) {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("/googleMap");
+		return modelAndView;
+	}
+	
+	
+	/*
+	@RequestMapping(value = "/googleMap")
+	public String googlemap(Locale locale, Model model) {
+		return "googleMap";
+	}
+	*/
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		return "index";
 	}
+	
+	
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
